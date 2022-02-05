28be76857276eac1fe46182f7244e6d1813404b9
Roddolfo Cabral Neves <roddas360@gmail.com>
2022-02-05 19:32:18 -0300

    Primeiras implementações de grafos

---
+++ grafo/direcionado/teste.py
@@ -0,0 +1,21 @@
+from ListaDeAdjacenciaGD import GrafoDirecionado 
+
+g = GrafoDirecionado()
+g.inserirVertice('a')
+g.inserirVertice('b')
+g.inserirVertice('c')
+g.inserirVertice('d')
+g.inserirVertice('e')
+g.inserirVertice('f')
+g.inserirVertice('g')
+
+g.inserirAresta('a','b')
+g.inserirAresta('b','e')
+g.inserirAresta('a','c')
+g.inserirAresta('a','d')
+g.inserirAresta('b','d')
+g.inserirAresta('c','d')
+g.inserirAresta('e','d')
+g.inserirAresta('f','g')
+
+print(g.mostrarGrafo())
