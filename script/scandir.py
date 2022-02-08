d44343cfb011273aacf2a0586c91f404f15c2e53
VitorLamego <vitorlamegoo@gmail.com>
2022-02-07 21:17:16 -0300

    Removendo código não utilizado

--- script/scandir.py
+++ script/scandir.py
@@ -1,12 +1,8 @@
-from sys import argv,exit
-from grafos import GrafoDirecionado, Path
+from grafos import GrafoDirecionado
 from utilitarios import Utilitarios
 
 utilitarios = Utilitarios()
 grafo = GrafoDirecionado()
-
-path1 = Path("pastas")
-path2 = Path("pastas")
 
 diretorio = utilitarios.uso()
 utilitarios.mapearDiretorio(diretorio, grafo)
